ca12b7516ddc579cfea5c509470029444392c1fc
Ilfat Aliev <ialiev685@gmail.com>
2022-02-13 15:28:24 +0300

    стилизация таблицы завершена

--- src/components/TableCalls/TableCalls.jsx
+++ src/components/TableCalls/TableCalls.jsx
@@ -47,64 +47,63 @@ export const TableCalls = ({ data }) => {
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Тип</th>
-          <th>Время</th>
-          <th>Сотрудник</th>
-          <th>Звонки</th>
-          <th>Источник</th>
-          <th>Оценка</th>
-          <th>Длительность</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data &&
-          data.map((item) => {
-            return (
-              <tr key={item.id}>
-                <td
-                  className="item__cells"
-                  data-v={item.status}
-                  data-b={item.in_out}
-                >
-                  {setTypeCall(item)}
-                </td>
-                <td className="item__cells">{getNormalizeTime(item.date)}</td>
-                <td
-                  className="item__cells"
-                  data-id={item.person_id}
-                  data-name={item.person_name}
-                  data-surname={item.person_surname}
-                >
-                  <img
-                    src={
-                      item.person_avatar ||
-                      "https://lk.skilla.ru/img/noavatar.jpg"
-                    }
-                    alt="аватар"
-                    width="32"
-                    height="32"
-                  />
-                </td>
-                <td>{identifyNumberPhone(item)}</td>
-                <td className="item__cells"></td>
-                <td
-                  className="item__cells"
-                  onClick={() => {
-                    const record = item.record;
-                    const partnership_id = item.partnership_id;
-                    fetchRecordCall({ record, partnership_id });
-                  }}
-                >
-                  {item.record !== "" ? "Прослушать" : ""}
-                </td>
-                <td className="item__cells">{getNormalizeMinute(item.time)}</td>
-              </tr>
-            );
-          })}
-      </tbody>
-    </table>
+    <div className="wrapper-table">
+      <table className="table">
+        <thead>
+          <tr className="table__item">
+            <th className="table__head-space"></th>
+            <th className="table__head-type">Тип</th>
+            <th className="table__head-time">Время</th>
+            <th className="table__head-person">Сотрудник</th>
+            <th className="table__head-calls">Звонки</th>
+            <th className="table__head-source">Источник</th>
+            <th className="table__head-score">Оценка</th>
+            <th className="table__head-duration">Длительность</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.map((item) => {
+              return (
+                <tr key={item.id} className="table__item">
+                  <td></td>
+                  <td data-v={item.status} data-b={item.in_out}>
+                    {setTypeCall(item)}
+                  </td>
+                  <td>{getNormalizeTime(item.date)}</td>
+                  <td
+                    className="item__cells"
+                    data-id={item.person_id}
+                    data-name={item.person_name}
+                    data-surname={item.person_surname}
+                  >
+                    <img
+                      src={
+                        item.person_avatar ||
+                        "https://lk.skilla.ru/img/noavatar.jpg"
+                      }
+                      alt="аватар"
+                      width="32"
+                      height="32"
+                    />
+                  </td>
+                  <td>{identifyNumberPhone(item)}</td>
+                  <td></td>
+                  <td
+                    onClick={() => {
+                      const record = item.record;
+                      const partnership_id = item.partnership_id;
+                      fetchRecordCall({ record, partnership_id });
+                    }}
+                  >
+                    {item.record !== "" ? "Прослушать" : ""}
+                  </td>
+                  <td>{getNormalizeMinute(item.time)}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
   );
 };
